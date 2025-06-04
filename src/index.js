bb73e45a22f5c57476299f193424560b9ffeae97
Sahil Singh <sahilsingh1892005@gmail.com>
2025-06-05 01:57:07 +0530

    some change

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-  import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { configureStore } from "@reduxjs/toolkit";
 import {Provider} from "react-redux";
 import { Toaster } from "react-hot-toast";
--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,4 +1,4 @@
-import React, { useDebugValue, useEffect, useState } from 'react'
+import React, {  useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom'
 import { apiConnector } from '../services/apiconnector';
